0e4b3302f92519ec20281a42e9bdcfd06ae17d3f
Ishwar-gupta <siddeshwargupta81@nepathyacollege.edu.np>
2025-11-19 19:54:46 +0545

    Delete plt02bar.py

--- plt02bar.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-product=['A','B','C','D']
-sales=[1000,1500,800,2000]
-plt.bar(product,sales,color='orange',label='Sales 2025')
-plt.xlabel("Product")
-plt.ylabel("Sales")
-plt.title("Product Sales Comparison")
-plt.legend(loc='upper left',fontsize=10)
-plt.show()
